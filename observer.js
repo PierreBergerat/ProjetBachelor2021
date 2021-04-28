669e87590ce11c748459560bbd791ea00c937ad7
Pierre Bergerat <pierre.bergerat@etu.unige.ch>
2021-04-28 19:02:26 +0200

    Full rework

--- observer.js
+++ observer.js
@@ -1,94 +1,87 @@
-/**
- * Returns the methods of an object's prototype
- * @param {prototype} prototype - the class prototype (passed via CLASSNAME.prototype) to target 
- * @returns - all the methods presents in the prototype
- */
-const getMethods = (prototype) => {
-  let properties = new Set(), currentObj = prototype;
-  do {
-    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
-  } while ((currentObj = Object.getPrototypeOf(currentObj)));
-  return [...properties.keys()].filter(item => typeof prototype[item] === 'function' && !["toString", "constructor"].includes(item));
-}
-
-/**
- * Augments a default method with another
- * @param {prototype} target - Object that possesses the to be replaced method
- * @param {Function} methodName - The method to replace
- * @param {Function} aspect - Function to add to the method
- * @param {"before"|"after"|"around"|"afterReturning"} advice - Where to add the method.
- */
-function replaceMethod(target, methodName, aspect, advice) {
-  const originalCode = target[methodName];
-  target[methodName] = (...args) => {
-    if (["before", "around"].includes(advice)) {
-      aspect.apply(target, [originalCode, methodName, args]);
-    }
-    const returnedValue = originalCode.apply(target, args);
-    if (["after", "around"].includes(advice)) {
-      aspect.apply(target, args);
-    }
-    if ("afterReturning" == advice) {
-      return aspect.apply(target, [returnedValue]);
-    }
-    return returnedValue;
+class Observer {
+  constructor(objects, namespaces, functions, blacklist) {
+    this.objects = objects;
+    this.namespaces = namespaces;
+    this.functions = functions;
+    this.blacklist = blacklist;
+    this.blacklist = new Array(...new Set([...this.blacklist, ...["toString", "setInterval", "setTimeout", "clearInterval"]]))
   }
-}
-
-/**
- * Injects a function into another
- * @param {prototype} target - Object that possesses the to be replaced method
- * @param {Function} aspect - Function to add to the method
- * @param {"before"|"after"|"around"|"afterReturning"} advice - Where to add the method
- */
-function inject(target, aspect, advice) {
-  const methods = getMethods(target);
-  methods.forEach(m => {
-    replaceMethod(target, m, aspect, advice);
-  })
-}
-
-/**
- * Example function to be run before another. This one logs everything into the console and calls the teacher's "log()" function.
- * @param  {...any} args - arguments passed to the initial function
- */
-function loggingAspect(...args) {
-  log(args[0], args[1], args[2])
-}
-
-/**
- * Example function to be run after the return of another function
- * @param {any} value - The returned value of the initial method
- */
-function loggingReturnedValueAspect(value) {
-  if (value != undefined) {
-    log(value)
+  /**
+   * Returns the methods of an object's prototype
+   * @param {prototype} prototype - the class prototype (passed via CLASSNAME.prototype) to target 
+   * @returns - all the methods presents in the prototype
+   */
+  getMethods = (prototype) => {
+    let properties = new Set(), currentObj = prototype;
+    do {
+      Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+    } while ((currentObj = Object.getPrototypeOf(currentObj)));
+    return [...properties.keys()].filter(item => typeof prototype[item] === 'function' && !["toString", "constructor"].includes(item));
   }
-  else {
-    log("undefined")
-  }
-  return value
-}
 
-/**
- * Injects every method of a namespace
- * @param {namespaceObject} namespaceObject - The namespaceObject whose functions will be augmented
- */
-function injectNamespace(namespaceObject) {
-  for (var name in namespaceObject) {
-    var potentialFunction = namespaceObject[name];
-    if (Object.prototype.toString.call(potentialFunction) === '[object Function]' && ![
-      "check", "inject", "addLoggingToNamespace", "getLoggableFunction", "loggingReturnedValueAspect", "loggingAspect", "replaceMethod", "getMethods", "injectFunctionPrototype", "toString", "setInterval", "wait", "setTimeout", "log", "display", "createArray", "updateObjects", "play", "clearInterval"
-    ].includes(potentialFunction.name)) {
-      replaceMethod(namespaceObject, name, loggingAspect, "before");
-      replaceMethod(namespaceObject, name, loggingReturnedValueAspect, "afterReturning")
+  /**
+   * Augments a default method with another
+   * @param {prototype} target - Object that possesses the to be replaced method
+   * @param {Function} methodName - The method to replace
+   * @param {Function} aspect - Function to add to the method
+   * @param {"before"|"after"|"around"|"afterReturning"} advice - Where to add the method.
+   */
+  replaceMethod(target, methodName, aspect, advice) {
+    const originalCode = target[methodName];
+    target[methodName] = (...args) => {
+      if (["before", "around"].includes(advice)) {
+        aspect.apply(target, [originalCode, methodName, args]);
+      }
+      const returnedValue = originalCode.apply(target, args);
+      if (["after", "around"].includes(advice)) {
+        aspect.apply(target, args);
+      }
+      if ("afterReturning" == advice) {
+        return aspect.apply(target, [returnedValue]);
+      }
+      return returnedValue;
     }
   }
-};
 
-/**
- * Starts the observation of the methods and functions calls
- */
-function startObserver() {
-  injectNamespace(globalThis);
+  /**
+   * Injects a function into another
+   * @param {prototype} target - Object that possesses the to be replaced method
+   * @param {Function} aspect - Function to add to the method
+   * @param {"before"|"after"|"around"|"afterReturning"} advice - Where to add the method
+   */
+  inject(target, functions) {
+    const methods = this.getMethods(target);
+    methods.forEach(m => {
+      for (let _function of functions) {
+        this.replaceMethod(target, m, _function.aspect, _function.advice);
+      }
+    })
+  }
+
+  /**
+   * Injects every method of a namespace
+   * @param {namespaceObject} namespaceObject - The namespaceObject whose functions will be augmented
+   */
+  injectNamespace(namespaceObject, functions) {
+    for (var name in namespaceObject) {
+      var potentialFunction = namespaceObject[name];
+      if (Object.prototype.toString.call(potentialFunction) === '[object Function]' && !this.blacklist.includes(potentialFunction.name)) {
+        for (let _function of functions) {
+          this.replaceMethod(namespaceObject, name, _function.aspect, _function.advice)
+        }
+      }
+    }
+  };
+
+  /**
+   * Starts the observation of the methods and functions calls
+   */
+  startObserver() {
+    for (let _object of this.objects) {
+      this.inject(_object.prototype, this.functions)
+    }
+    for (let _namespace of this.namespaces) {
+      this.injectNamespace(_namespace, this.functions)
+    }
+  }
 }
--- teacher.js
+++ teacher.js
@@ -1,9 +1,9 @@
-var logs = []; // container for the logs
-var curr = -1; // currently displayed log index
-const displayContainer = document.getElementById('display');
-const algorithm = document.getElementById('algorithm');
-var actionListeners = new Map();
-var afterListeners = new Map();
+var _logs = []; // container for the logs
+var _curr = -1; // currently displayed log index
+const _displayContainer = document.getElementById('display');
+const _algorithm = document.getElementById('algorithm');
+var _actionListeners = new Map();
+var _afterListeners = new Map();
 
 class Utils {
     /**
@@ -36,51 +36,6 @@ class Utils {
 }
 
 /**
- * Check will be able to verify the values/names/code of the current function and to interrupt it or react accordingly to the values read
- * @param {Function} func - The function that has been called
- * @param {String} name - The name of the function
- * @param {IArguments} args - the function's arguments
- */
-function log(func, name, args) {
-    if (name && args) {
-        logs.push([name.toString() + '()', Utils.deepCopy(args), func.toString()]);
-    } else {
-        for (let i = logs.length - 1; i >= 0; i--) {
-            if (logs[i].length != 4) {
-                logs[i].push(Utils.deepCopy(func));
-                break;
-            }
-        }
-    }
-}
-
-/**
- * Triggers the functions linked to the listeners
- * @param {Number} curr - The index of the currently displayed log
- * @param {Boolean} isGoingForward - Whether display is going forward or backward
- */
-function updateObjects(curr, isGoingForward) {
-    if (curr > 0) {
-        lastLog = logs[curr - 1];
-        for (let obj of afterListeners.get(lastLog[0].split('()')[0]) || []) {
-            if (obj[0][obj[1]]) { // if the object calling has the function as a method
-                obj[0][obj[1]](lastLog, isGoingForward); // then it is called as object.method()
-            } else { // otherwise the function is called globally and a reference to obj[0] is passed
-                obj[1].call(this, obj[0], lastLog, isGoingForward);
-            }
-        }
-    }
-    currentLog = logs[curr];
-    for (let obj of actionListeners.get(currentLog[0].split('()')[0]) || []) {
-        if (obj[0][obj[1]]) {
-            obj[0][obj[1]](currentLog, isGoingForward);
-        } else {
-            obj[1].call(this, obj[0], currentLog, isGoingForward);
-        }
-    }
-}
-
-/**
  * Is used to implement listeners in any classes extending TItem.
  */
 class TItem {
@@ -94,7 +49,7 @@ class TItem {
             return;
         }
         for (let [funcName, func] of beforeAction) {// Adds / Appends an array of form [object, function] to actionListeners
-            actionListeners.get(funcName)?.push([this, func]) || actionListeners.set(funcName, [[this, func]]);
+            _actionListeners.get(funcName)?.push([this, func]) || _actionListeners.set(funcName, [[this, func]]);
         }
     }
 
@@ -103,19 +58,19 @@ class TItem {
             return;
         }
         for (let [funcName, func] of afterAction) {
-            afterListeners.get(funcName)?.push([this, func]) || afterListeners.set(funcName, [[this, func]]);
+            _afterListeners.get(funcName)?.push([this, func]) || _afterListeners.set(funcName, [[this, func]]);
         }
     }
 
     runBeforeFunction(beforeAction) {
         for (let [funcName, func] of beforeAction) {
-            actionListeners.get(funcName)?.push([this, func]) || actionListeners.set(funcName, [[this, func]]);
+            _actionListeners.get(funcName)?.push([this, func]) || _actionListeners.set(funcName, [[this, func]]);
         }
     }
 
     runAfterFunction(afterAction) {
         for (let [funcName, func] of afterAction) {
-            afterListeners.get(funcName)?.push([this, func]) || afterListeners.set(funcName, [[this, func]]);
+            _afterListeners.get(funcName)?.push([this, func]) || _afterListeners.set(funcName, [[this, func]]);
         }
     }
 }
@@ -172,107 +127,159 @@ class TArray extends TItem {
         if (typeof indexes === 'number') {
             indexes = [indexes];
         }
+        if (indexes.includes(-1)) {
+            return
+        }
         for (let index of indexes) {
-            items[index].classList.add(className);
+            try {
+                items[index].classList.add(className);
+            }
+            catch (e) {
+                console.log(`indexes : ${indexes}`);
+                console.log(e);
+            }
         }
     }
 }
 
-/**
- * Displays the current log to the screen in a preformated placeholder
- * @param {Boolean} shouldGoForward - Indicates whether the display should show the next or the previous state
- */
-function display(shouldGoForward = true) {
-    if (curr != -1) {
-        if (shouldGoForward) {
-            curr < (logs.length - 1) ? curr += 1 : curr;
+class Teacher {
+    /**
+     * Check will be able to verify the values/names/code of the current function and to interrupt it or react accordingly to the values read
+     * @param {Function} func - The function that has been called
+     * @param {String} name - The name of the function
+     * @param {IArguments} args - the function's arguments
+     */
+    static log(func, name, args) {
+        if (name && args) {
+            _logs.push([name.toString() + '()', Utils.deepCopy(args), func.toString()]);
         } else {
-            curr > 0 ? curr -= 1 : curr;
+            for (let i = _logs.length - 1; i >= 0; i--) {
+                if (_logs[i].length != 4) {
+                    _logs[i].push(Utils.deepCopy(func));
+                    break;
+                }
+            }
         }
-    } else {// Init
-        new TArray([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 4891, 455, 23, 234, 213],
-            'table',
-            [
+    }
+
+    /**
+     * Triggers the functions linked to the listeners
+     * @param {Number} curr - The index of the currently displayed log
+     * @param {Boolean} isGoingForward - Whether display is going forward or backward
+     */
+    static updateObjects(curr, isGoingForward) {
+        if (curr > 0) {
+            let lastLog = _logs[curr - 1];
+            for (let obj of _afterListeners.get(lastLog[0].split('()')[0]) || []) {
+                if (obj[0][obj[1]]) { // if the object calling has the function as a method
+                    obj[0][obj[1]](lastLog, isGoingForward); // then it is called as object.method()
+                } else { // otherwise the function is called globally and a reference to obj[0] is passed
+                    obj[1].call(this, obj[0], lastLog, isGoingForward);
+                }
+            }
+        }
+        let currentLog = _logs[curr];
+        for (let obj of _actionListeners.get(currentLog[0].split('()')[0]) || []) {
+            if (obj[0][obj[1]]) {
+                obj[0][obj[1]](currentLog, isGoingForward);
+            } else {
+                obj[1].call(this, obj[0], currentLog, isGoingForward);
+            }
+        }
+    }
+
+    /**
+     * Displays the current log to the screen in a preformated placeholder
+     * @param {Boolean} shouldGoForward - Indicates whether the display should show the next or the previous state
+     */
+    static display(shouldGoForward = true) {
+        if (_curr != -1) {
+            if (shouldGoForward) {
+                _curr < (_logs.length - 1) ? _curr += 1 : _curr;
+            } else {
+                _curr > 0 ? _curr -= 1 : _curr;
+            }
+        } else {// Init
+            new TArray([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 4891, 455, 23, 234, 213],
+                'table',
                 [
-                    'swap', (that, log) => {
-                        that.select([log[1][1], log[1][1] + 1], 'red');
-                    }
-                ],
-                [
-                    'isSmaller', (that, log) => {
-                        let index = Utils.findSubArray(that.refArray, log[1]);
-                        if (index == -1) {
-                            index = Utils.findSubArray(that.refArray, Utils.deepCopy(log[1]).reverse());
-                            that.select([index, index + 1]);
-                        } else {
-                            that.select([index, index + 1]);
+                    [
+                        'swap', (that, log) => {
+                            that.select([log[1][1], log[1][1] + 1], 'red');
                         }
-                    }
-                ]
-            ],
-            [
-                [
-                    'swap', (that, log) => { that.updateArray(log[3]) }
+                    ],
+                    [
+                        'isSmaller', (that, log) => {
+                            let index = Utils.findSubArray(that.refArray, log[1]);
+                            if (index == -1) {
+                                index = Utils.findSubArray(that.refArray, Utils.deepCopy(log[1]).reverse());
+                                that.select([index, index + 1]);
+                            } else {
+                                that.select([index, index + 1]);
+                            }
+                        }
+                    ]
                 ],
                 [
-                    'isSmaller', 'displayArray'
-                ]
-            ]);
-        curr++;
-        algorithm.innerHTML = `<pre>${logs[0][2]}</pre>`;
-    }
-    updateObjects(curr, shouldGoForward);
-    algorithm.innerHTML = '<pre>' + algorithm.innerHTML
-        .replaceAll('<span>', '')
-        .replaceAll('</span>', '')
-        .replaceAll('<pre>', '')
-        .replaceAll('</pre>', '')
-        .split(logs[curr][0].replaceAll(')', ''))
-        .join(`</pre><span>${logs[curr][0].replaceAll('()', '')}</span><pre>(`) + '</pre>';
-    displayContainer.innerHTML = "";
-    for (let elem in logs[curr]) {
-        let p = document.createElement('pre');
-        p.classList.add('m-0');
-        switch (elem) {
-            case '0': p.innerText = "Fonction : "; break;
-            case '1': p.innerText = "Arguments : "; break;
-            case '2': p.innerText = "Code : "; break;
-            case '3': p.innerText = "Retourne : "; break;
-            default: break;
+                    [
+                        'swap', (that, log) => { that.updateArray(log[3]) }
+                    ]
+                ]);
+            _curr++;
+            _algorithm.innerHTML = `<pre>${_logs[0][2]}</pre>`;
         }
-        if (elem % 2 != 0) {
-            p.innerHTML += JSON.stringify(logs[curr][elem]);
-        } else {
-            p.innerHTML += logs[curr][elem];
+        Teacher.updateObjects(_curr, shouldGoForward);
+        _algorithm.innerHTML = '<pre>' + _algorithm.innerHTML
+            .replaceAll('<span>', '')
+            .replaceAll('</span>', '')
+            .replaceAll('<pre>', '')
+            .replaceAll('</pre>', '')
+            .split(_logs[_curr][0].replaceAll(')', ''))
+            .join(`</pre><span>${_logs[_curr][0].replaceAll('()', '')}</span><pre>(`) + '</pre>';
+        _displayContainer.innerHTML = "";
+        for (let elem in _logs[_curr]) {
+            let p = document.createElement('pre');
+            p.classList.add('m-0');
+            switch (elem) {
+                case '0': p.innerText = "Fonction : "; break;
+                case '1': p.innerText = "Arguments : "; break;
+                case '2': p.innerText = "Code : "; break;
+                case '3': p.innerText = "Retourne : "; break;
+                default: break;
+            }
+            if (elem % 2 != 0) {
+                p.innerHTML += JSON.stringify(_logs[_curr][elem]);
+            } else {
+                p.innerHTML += _logs[_curr][elem];
+            }
+            _displayContainer.appendChild(p);
         }
-        displayContainer.appendChild(p);
     }
-}
 
-/**
- * Automatically plays the next log until the end or until the button is pressed again
- * @param {string} id - the id of the start / stop button
- */
-function play(id) {
-    id = id.target ? id.target?.id : id;
-    let clickHandler = function () {
-        shouldRun = false;
-    }
-    let shouldRun = true;
-    let button = document.getElementById(id);
-    let textBackup = button.innerText;
-    button.innerText = "Stopper la séquence";
-    button.removeAttribute("onclick");
-    button.removeEventListener('click', play);
-    button.addEventListener('click', clickHandler);
-
-    var intervalId = window.setInterval(function () {
-        display(true);
-        if (curr == logs.length - 1 || !shouldRun) {
-            clearInterval(intervalId);
-            button.removeEventListener('click', clickHandler);
-            button.innerText = textBackup;
-            button.addEventListener('click', play);
+    /**
+     * Automatically plays the next log until the end or until the button is pressed again
+     * @param {string} id - the id of the start / stop button
+     */
+    static play(id) {
+        id = id.target ? id.target?.id : id;
+        let clickHandler = function () {
+            shouldRun = false;
         }
-    }, 500);
+        let shouldRun = true;
+        let button = document.getElementById(id);
+        let textBackup = button.innerText;
+        button.innerText = "Stopper la séquence";
+        button.removeAttribute("onclick");
+        button.removeEventListener('click', Teacher.play);
+        button.addEventListener('click', clickHandler, { once: true });
+
+        var intervalId = window.setInterval(function () {
+            Teacher.display(true);
+            if (_curr == _logs.length - 1 || !shouldRun) {
+                clearInterval(intervalId);
+                button.innerText = textBackup;
+                button.addEventListener('click', Teacher.play);
+            }
+        }, 500);
+    }
 }
